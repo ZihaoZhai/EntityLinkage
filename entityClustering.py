36d59bba8a7d1594d2a4cdbeed17c7b60c3fd75c
Zihao Zhai <zihaozha@usc.edu>
2018-03-29 14:14:08 -0700

    update code

---
+++ entityClustering.py
@@ -0,0 +1,74 @@
+import sys
+import json
+import os
+import sys
+reload(sys) 
+sys.setdefaultencoding('utf8')
+packagePath='../rltk'
+inputPath=sys.argv[1]
+outputPath=sys.argv[2]
+if len(sys.argv)==4:
+	packagePath=sys.argv[3]
+sys.path.append(packagePath)
+sys.path.append('/Users/zihaozhai/Desktop/LORELEI/EntityLinkage/functions')
+import rltk
+import entityLinkageFunctions
+tk = rltk.init()
+topicWords=[]
+
+groundTruthPath='../groundTruthOutputJsonData/'
+
+wordsList={"PER":[],"ORG":[],"GPE":[],"LOC":[]}
+stringSimilarity='trigram_jaccard_similarity'
+#stringSimilarity='jaccard_index_similarity'
+#stringSimilarity='jaro_winkler_similarity'
+#stringSimilarity='levenshtein_similarity'
+#stringSimilarity='monge_elkan_similarity'
+#stringSimilarity='needleman_wunsch_similarity'
+#stringSimilarity='string_cosine_similarity'
+#stringSimilarity='symmetric_monge_elkan_similarity'
+ind=0
+if os.listdir(inputPath)[ind]=='.DS_Store':
+	ind+=1
+files=os.listdir(inputPath)
+if 'll_nepal_out' in os.listdir(inputPath)[ind]:
+	for f in files:
+		if f!='.DS_Store':
+			input=open(inputPath+f)
+			data=input.read()
+			data=json.loads(data)
+			for t in wordsList:
+				if data.has_key(t):
+					for tw in data[t]:
+						wordsList[t].append((tw,f))
+elif 'AWATD' in os.listdir(inputPath)[ind]:
+	for f in files:
+		if f!='.DS_Store':
+			input=open(inputPath+f)
+			data=input.read()
+			data=json.loads(data)
+			for t in wordsList:
+				if data["_source"].has_key(t):
+					for tw in data["_source"][t]:
+						wordsList[t].append((tw,f))
+thr=0.3
+topicWords=entityLinkageFunctions.pickTopicWords(wordsList,thr,stringSimilarity)
+#thr=0.85
+#topicWords=entityLinkageFunctions.spectralClustering(wordsList,stringSimilarity,groundTruthPath,thr)
+if not os.path.exists(outputPath):
+	os.makedirs(outputPath)
+for tp in topicWords:
+	for t in topicWords[tp]:
+		if len(t['originalNames'])>1:
+			print t['preferredName']
+			print '-----------------'
+			for on in t['originalNames']:
+				print on['name'],len(on['docIds'])
+			print '\n'
+		newData=json.dumps(t)
+		ouputFile=outputPath+t['preferredName'].replace('.json','').replace('/','').encode('utf-8')+'_'+t['type'].encode('utf-8')+'.json'
+		output=open(ouputFile,'w')
+		output.write(newData)
+		output.close()
+
+
--- functions/entityLinkageFunctions.py
+++ functions/entityLinkageFunctions.py
@@ -1,12 +1,30 @@
 import sys
+import os
+import json
+import copy
+import matplotlib.pyplot as plt
 packagePath='rltk'
 sys.path.append(packagePath)
 import rltk
+from sklearn.cluster import SpectralClustering
 tk = rltk.init()
 reload(sys) 
 sys.setdefaultencoding('utf8')
 
-def threeCharTkenJaccardSimilarity(strA,strB,tokenLen):
+def readGroundTruth(groundTruthPath):
+	groundTruthPairSet={'PER':set(),'LOC':set(),'GPE':set(),'ORG':set()}
+	groundTruthFiles=os.listdir(groundTruthPath)
+	for f in groundTruthFiles:
+		input=open(groundTruthPath+'/'+f,'r')
+		data=json.loads(input.read())
+		for i in xrange(len(data['originalNames'])):
+			for j in xrange(i+1,len(data['originalNames'])):
+				groundTruthPairSet[data['type']].add((data['originalNames'][i]['name'],data['originalNames'][j]['name']))
+	return groundTruthPairSet
+
+def n_char_token_jaccard_similarity(strA,strB,tokenLen):
+	strA='#'*(tokenLen-1)+strA+'#'*(tokenLen-1)
+	strB='#'*(tokenLen-1)+strB+'#'*(tokenLen-1)
 	if len(strA)<tokenLen:
 		setA=set([strA])
 	else:
@@ -22,7 +40,7 @@ def threeCharTkenJaccardSimilarity(strA,strB,tokenLen):
 	similarity=tk.jaccard_index_similarity(setA, setB)
 	return similarity
 
-def pickTopicWordsFromNepal(wordsList,thr):
+def pickTopicWords(wordsList,thr,stringSimilarity):
 	topicWords={"PER":[],"LOC":[],"GPE":[],"ORG":[]}
 	for tp in wordsList:
 		for d in wordsList[tp]:
@@ -36,14 +54,22 @@ def pickTopicWordsFromNepal(wordsList,thr):
 						haveName=1
 						break
 					else:
-						similarity=threeCharTkenJaccardSimilarity(n['name'].lower(),d[0].lower(),3)
-						#similarity=tk.jaccard_index_similarity(set(n['name'].lower().split(' ')), set(d[0].lower().split(' ')))
-						#similarity=tk.jaro_winkler_similarity(n['name'].lower(), d[0].lower(), threshold=0.7, scaling_factor=0.1, prefix_len=4)
-						#similarity=tk.levenshtein_similarity(n['name'].encode().lower(), d[0].encode().lower())
-						#similarity=tk.monge_elkan_similarity(n['name'].lower().split(' '),d[0].lower().split(' '))
-						#similarity=tk.needleman_wunsch_similarity(n['name'].lower(),d[0].lower())
-						#similarity=tk.string_cosine_similarity(n['name'].lower().split(' '),d[0].lower().split(' '))
-						#similarity=tk.symmetric_monge_elkan_similarity(n['name'].lower().split(' '),d[0].lower().split(' '))
+						if stringSimilarity=='trigram_jaccard_similarity':
+							similarity=n_char_token_jaccard_similarity(n['name'].lower(),d[0].lower(),3)
+						elif stringSimilarity=='jaccard_index_similarity':
+							similarity=tk.jaccard_index_similarity(set(n['name'].lower().split(' ')), set(d[0].lower().split(' ')))
+						elif stringSimilarity=='jaro_winkler_similarity':
+							similarity=tk.jaro_winkler_similarity(n['name'].lower(), d[0].lower(), threshold=0.7, scaling_factor=0.1, prefix_len=4)
+						elif stringSimilarity=='levenshtein_similarity':
+							similarity=tk.levenshtein_similarity(n['name'].encode().lower(), d[0].encode().lower())
+						elif stringSimilarity=='monge_elkan_similarity':
+							similarity=tk.monge_elkan_similarity(n['name'].lower().split(' '),d[0].lower().split(' '))
+						elif stringSimilarity=='needleman_wunsch_similarity':
+							similarity=tk.needleman_wunsch_similarity(n['name'].lower(),d[0].lower())
+						elif stringSimilarity=='string_cosine_similarity':
+							similarity=tk.string_cosine_similarity(n['name'].lower().split(' '),d[0].lower().split(' '))
+						elif stringSimilarity=='symmetric_monge_elkan_similarity':
+							similarity=tk.symmetric_monge_elkan_similarity(n['name'].lower().split(' '),d[0].lower().split(' '))
 						if similarity>mostSimilar[0] and similarity>=thr:
 							mostSimilar[0]=similarity
 							mostSimilar[1]=ele
@@ -56,8 +82,8 @@ def pickTopicWordsFromNepal(wordsList,thr):
 							'docIds':[d[1]]
 						}
 					mostSimilar[1]['originalNames'].append(obj)
-					if len(d[0])>len(mostSimilar[1]['preferredName']):
-						mostSimilar[1]['preferredName']=d[0]
+					#if len(d[0])>len(mostSimilar[1]['preferredName']):
+					#	mostSimilar[1]['preferredName']=d[0]
 				else:
 					obj={
 						"preferredName" : d[0],
@@ -70,54 +96,149 @@ def pickTopicWordsFromNepal(wordsList,thr):
 						]
 					}
 					topicWords[tp].append(obj)
+	for tp in topicWords:
+		for i in xrange(len(topicWords[tp])):
+			topicWords[tp][i]['originalNames'].sort(key=lambda x:x['name'])
+			maxLen=0
+			maxName=''
+			for on in topicWords[tp][i]['originalNames']:
+				if len(on['docIds'])>maxLen:
+					maxLen=len(on['docIds'])
+					maxName=on['name']
+			topicWords[tp][i]['preferredName']=maxName
 	return topicWords
 
-def pickTopicWordsFromHaiti(wordsList,thr):
-	topicWords={"PER":[],"LOC":[],"GPE":[],"ORG":[]}
+def spectralClustering(wordsList,stringSimilarity,groundTruthPath,thr):
+	groundTruthFiles=os.listdir(groundTruthPath)
+	typeClusterNum={'PER':0,'ORG':0,'LOC':0,'GPE':0}
+	for gtf in groundTruthFiles:
+		if gtf!='.DS_Store':
+			input=open(groundTruthPath+gtf,'r')
+			data=input.read()
+			data=json.loads(data)
+			typeClusterNum[data['type']]+=1
+	matrix={'PER':[],'LOC':[],'GPE':[],'ORG':[]}
+	clusters={'PER':[],'LOC':[],'GPE':[],'ORG':[]}
+	wordsSet={'PER':set([]),'LOC':set([]),'GPE':set([]),'ORG':set([])}
+	clusterLabelDic={'PER':{},'LOC':{},'ORG':{},'GPE':{}}
 	for tp in wordsList:
-		for d in wordsList[tp]:
-			#print tp,d
-			haveName=0
-			mostSimilar=[-1,None]
-			for ele in topicWords[tp]:
-				for n in ele['originalNames']:
-					if n['name']==d[0]:
-						n['docIds'].append(d[1])
-						haveName=1
-						break
+		for enti in wordsList[tp]:
+			wordsSet[tp].add(enti[0])
+		wordsSet[tp]=sorted(list(wordsSet[tp]))
+	'''
+	tyy='PER'
+	for w1 in wordsSet[tyy]:
+		row=[]
+		for w2 in wordsSet[tyy]:
+			similarity=n_char_token_jaccard_similarity(w1.lower(),w2.lower(),3)
+			similarity=abs(0) if similarity<thr else similarity
+			row.append(similarity)
+		matrix[tp].append(row)
+	spectral=SpectralClustering(n_clusters=typeClusterNum[tp],affinity="precomputed")
+	clusterLabels=spectral.fit_predict(matrix[tp])
+	print len(clusterLabels)
+	'''
+	for tp in wordsSet:
+		num=0
+		for w1 in wordsSet[tp]:
+			row=[]
+			for w2 in wordsSet[tp]:
+				if stringSimilarity=='trigram_jaccard_similarity':
+					similarity=n_char_token_jaccard_similarity(w1.lower(),w2.lower(),3)
+				elif stringSimilarity=='jaccard_index_similarity':
+					similarity=tk.jaccard_index_similarity(set(w1.lower().split(' ')), set(w2.lower().split(' ')))
+				elif stringSimilarity=='jaro_winkler_similarity':
+					similarity=tk.jaro_winkler_similarity(w1.lower(), w2.lower(), threshold=0.7, scaling_factor=0.1, prefix_len=4)
+				elif stringSimilarity=='levenshtein_similarity':
+					similarity=tk.levenshtein_similarity(w1.encode().lower(), w2.encode().lower())
+				elif stringSimilarity=='monge_elkan_similarity':
+					similarity=tk.monge_elkan_similarity(w1.lower().split(' '),w2.lower().split(' '))
+				elif stringSimilarity=='needleman_wunsch_similarity':
+					if w1==w2:
+						similarity=1
 					else:
-						similarity=threeCharTkenJaccardSimilarity(n['name'].lower(),d[0].lower(),3)
-						#similarity=tk.jaccard_index_similarity(set(n['name'].lower().split(' ')), set(d[0].lower().split(' ')))
-						#similarity=tk.jaro_winkler_similarity(n['name'].lower(), d[0].lower(), threshold=0.7, scaling_factor=0.1, prefix_len=4)
-						#similarity=tk.levenshtein_similarity(n['name'].encode().lower(), d[0].encode().lower())
-						#similarity=tk.monge_elkan_similarity(n['name'].lower().split(' '),d[0].lower().split(' '))
-						#similarity=tk.needleman_wunsch_similarity(n['name'].lower(),d[0].lower())
-						#similarity=tk.string_cosine_similarity(n['name'].lower().split(' '),d[0].lower().split(' '))
-						#similarity=tk.symmetric_monge_elkan_similarity(n['name'].lower().split(' '),d[0].lower().split(' '))
-						if similarity>mostSimilar[0] and similarity>=thr:
-							mostSimilar[0]=similarity
-							mostSimilar[1]=ele
-				if haveName==1:
+						similarity=tk.needleman_wunsch_similarity(w1.lower(),w2.lower())
+				elif stringSimilarity=='string_cosine_similarity':
+					similarity=tk.string_cosine_similarity(w1.lower().split(' '),w2.lower().split(' '))
+				elif stringSimilarity=='symmetric_monge_elkan_similarity':
+					similarity=tk.symmetric_monge_elkan_similarity(w1.lower().split(' '),w2.lower().split(' '))
+				#if similarity<thr:
+				#	similarity=0
+				similarity=0 if similarity<thr and similarity!=0 else similarity
+				row.append(similarity)
+
+			#print num,row
+			num+=1
+			matrix[tp].append(row)
+		#print '\n'
+		#print tp,len(matrix[tp])
+		#spectral=SpectralClustering(n_clusters=typeClusterNum[tp],affinity="precomputed")
+		spectral=SpectralClustering(n_clusters=typeClusterNum[tp],affinity="precomputed",eigen_solver='arpack')
+		clusterLabels=spectral.fit_predict(matrix[tp])
+		#print len(clusterLabels),len(set(clusterLabels)),max(clusterLabels)
+		for i in range(len(clusterLabels)):
+			clusterLabelDic[tp][wordsSet[tp][i]]=int(clusterLabels[i])
+		for i in xrange(len(set(clusterLabels))):
+			obj={
+				'preferredName':'',
+				'type':tp,
+				'originalNames':[]
+			}
+			clusters[tp].append(obj)
+		#print len(clusters[tp])
+		#print len(clusterLabels),len(matrix[tp]),len(clusters[tp]),len(clusterLabelDic[tp]),len(wordsSet[tp]),len(wordsList[tp])
+		for w in wordsList[tp]:	
+			#print clusters[tp][clusterLabelDic[tp][w[0]]]
+			if len(w[0])>len(clusters[tp][clusterLabelDic[tp][w[0]]]['preferredName']):
+				clusters[tp][clusterLabelDic[tp][w[0]]]['preferredName']=w[0]
+			hav=0
+			for on in clusters[tp][clusterLabelDic[tp][w[0]]]['originalNames']:
+				if on['name']==w[0]:
+					on['docIds'].append(w[1])
+					hav=1
 					break
-			if haveName==0:
-				if mostSimilar[0]!=-1:
-					obj={
-							'name':d[0],
-							'docIds':[d[1]]
-						}
-					mostSimilar[1]['originalNames'].append(obj)
-					if len(d[0])>len(mostSimilar[1]['preferredName']):
-						mostSimilar[1]['preferredName']=d[0]
-				else:
-					obj={
-						"preferredName" : d[0],
-						"type":tp,
-						"originalNames":[
-							{
-								"name":d[0],
-								"docIds":[d[1]]
-							}
-						]
-					}
-					topicWords[tp].append(obj)
-	return topicWords
+			if hav==0:
+				obj={
+					'name':w[0],
+					'docIds':[w[1]]
+				}
+				clusters[tp][clusterLabelDic[tp][w[0]]]['originalNames'].append(obj)
+		for tp in clusters:
+			for i in xrange(len(clusters[tp])):
+				clusters[tp][i]['originalNames'].sort(key=lambda x:x['name'])
+	return clusters
+
+def drawGraph(performanceResult):
+	plt.subplot2grid((9,9),(0,0),rowspan=4,colspan=8)
+	print len(performanceResult["thresholdsList"]),len(performanceResult["fScoreList"]['PER'])
+	plt.plot(performanceResult["thresholdsList"], performanceResult["fScoreList"]['PER'], '.-',label='PER')
+	plt.plot(performanceResult["thresholdsList"], performanceResult["fScoreList"]['ORG'], '.-',label='ORG')
+	plt.plot(performanceResult["thresholdsList"], performanceResult["fScoreList"]['LOC'], '.-',label='LOC')
+	plt.plot(performanceResult["thresholdsList"], performanceResult["fScoreList"]['GPE'], '.-',label='GPE')
+	plt.axis([0, 1, 0, 1])
+	plt.ylabel('F-Score')
+	plt.xlabel('Threshold')
+	plt.legend(bbox_to_anchor=(1.03, 1), loc=2, borderaxespad=0)
+	plt.grid(True)
+	plt.title('Performance Curve')
+	plt.subplot2grid((9,9),(5,0),rowspan=4,colspan=8)
+	plt.plot(performanceResult["recallList"]['PER'], performanceResult["precisionList"]['PER'], '.-',label='PER')
+	plt.plot(performanceResult["recallList"]['ORG'], performanceResult["precisionList"]['ORG'], '.-',label='ORG')
+	plt.plot(performanceResult["recallList"]['LOC'], performanceResult["precisionList"]['LOC'], '.-',label='LOC')
+	plt.plot(performanceResult["recallList"]['GPE'], performanceResult["precisionList"]['GPE'], '.-',label='GPE')
+	plt.axis([0, 1, 0, 1])
+	plt.xlabel('Recall')
+	plt.ylabel('Precision')
+	plt.legend(bbox_to_anchor=(1.03, 1), loc=2, borderaxespad=0)
+	plt.grid(True)
+	plt.show()
+		
+
+	
+	
+
+
+
+
+
+
